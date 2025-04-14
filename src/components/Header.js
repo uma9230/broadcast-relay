d4a2ee616c9569deee26ce67a12a70855b1b44b3
Abdulqadir <abdulqadir.b8604@gmail.com>
2025-04-15 00:37:01 +0530

    change 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ function Header() {
     return (
         <header>
             <img src="https://yt3.googleusercontent.com/fkBqLGX5E_WMfwO62pXX4bEd3U7StuUF9oTtgfxTj5q-dqFzQaTR8XKMVcp8en_ICcs1xbbWGQ=s176-c-k-c0x00ffffff-no-rj" alt="Logo" className="logo" />
-            <h1>Broadcast Service</h1>
+            <h1>Nova Cast</h1>
             <h2>TYBCA</h2>
             <h2>{`${miqaatName}`}</h2>
         </header>
